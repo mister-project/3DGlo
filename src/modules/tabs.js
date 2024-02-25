debb84d9888c97c8ba7b8c69f03bec1e88b77bf4
Yuriy Kulikov <kulikov-y@ya.ru>
2024-02-25 12:38:13 +0300

    Табы - промежуточное сохранение

--- src/modules/tabs.js
+++ src/modules/tabs.js
@@ -1,4 +1,22 @@
 const tabs = () => {
-    console.log('tabs')
+    const tabPanel = document.querySelector('.service-header')
+    const tabs = document.querySelectorAll('.service-header-tab')
+    const tabContent = document.querySelectorAll('.service-tab')
+    tabPanel, addEventListener('click', (e) => {
+        console.log(e.target.closest('.service-header-tab'))
+
+        if (e.target.classList.contains('service-header-tab')) {
+            tabs.forEach((tab, index) => {
+                if (tab === e.target) {
+                    tab.classList.add('active')
+                } else {
+                    tab.classList.remove('active')
+                }
+
+            })
+        }
+    })
+
+
 }
 export default tabs
