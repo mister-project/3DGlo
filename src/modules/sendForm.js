e0d1ba4a5ed3c191c326d915e74f347cd779812f
Yuriy Kulikov <kulikov-y@yandex.ru>
2024-03-18 17:47:11 +0000

    9-00

--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -2,10 +2,18 @@ const sendForm = (idForm) => {
     console.dir(idForm)
     const form = document.getElementById(idForm)
 
+    let user = {
+        name: 'Alex',
+        age: 24
+    }
+
     const sendData = (data) => {
         return fetch('https://jsonplaceholder.typicode.com/posts', {
             method: 'POST',
-            body: data
+            body: data,
+            headers: {
+                "Content-Type": "multipart/form-data"
+            }
 
         }).then(res => res.json())
 
@@ -15,9 +23,13 @@ const sendForm = (idForm) => {
 
     form.addEventListener('submit', (e) => {
         e.preventDefault()
+
+        const formData = new FormData(form)
         console.log('submit');
 
-        sendData({}).then()
+        sendData(formData).then(data => {
+            console.log(data);
+        })
     })
 }
 export default sendForm
