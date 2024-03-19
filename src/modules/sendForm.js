abed5dc9e3f1c5e23c9e3b32c3c96f4163d0b806
Yuriy Kulikov <kulikov-y@ya.ru>
2024-03-19 22:00:53 +0300

    23-25 работает отправка и очищаются поля

--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -1,9 +1,25 @@
-const sendForm = ({ formId, someElem = [] }) => {
+const sendForm = ({
+    formId,
+    someElem = []
+}) => {
     // console.dir(idForm)
     const form = document.getElementById(formId)
-//Функция для валидации полей
+
+    //Функция для валидации полей
     const validate = (list) => {
-        console.log(list)
+        let success = true
+
+
+
+
+        // list.forEach(input => {
+        //     if (!input.classList.contains('success')) {
+        //         success = false
+        //     }
+
+        // })
+
+        return success
     }
 
     const sendData = (data) => {
@@ -18,40 +34,49 @@ const sendForm = ({ formId, someElem = [] }) => {
 
     form.addEventListener('submit', (e) => {
         e.preventDefault() //отменяем действие браузера по умолчанию при нажатии на кнопку
-//Получаем NodeList из полей ***для валидации***
-        const formElements = form.querySelectorAll('input');
 
-//получаем сразу все поля из формы
+        //Получаем NodeList из полей ***для валидации***
+        const formElements = form.querySelectorAll('input');
+        console.log(formElements)
+
+        //получаем сразу все поля из формы
         const formData = new FormData(form)
         const formBody = {} //сюда будем собирать инфу из формы и калькулятора
 
-//перебираем и формируем заново получившиеся поля из формы
+        //перебираем и формируем заново получившиеся поля из формы
         formData.forEach((val, key) => {
             formBody[key] = val
 
         })
-//вывод и отправка значения "Калькулятора"
+        //вывод и отправка значения "Калькулятора"
         someElem.forEach(elem => {
-        const element = document.getElementById(elem.id)
+            const element = document.getElementById(elem.id)
 
-        console.log(element)
-         
-         if(elem.type === 'block') {
-         formBody[elem.id] = element.textContent         
-         }else if (elem.type === 'input') {
-         formBody[elem.id] = element.value
-         
-         }
+
+            if (elem.type === 'block') {
+                formBody[elem.id] = element.textContent
+            } else if (elem.type === 'input') {
+                formBody[elem.id] = element.value
+            }
 
         })
         console.log('submit');
-        
-        validate(formElements)
+        //отправка данных формы, если поля прошли валидацию
+        if (validate(formElements)) {
+            sendData(formBody).then(data => {
+                formElements.forEach(input => {
 
-        sendData(formBody).then(data => {            
+                    input.value = '' //очищаем поля после отправки данных
 
-            console.log(data);
-        })
+                })
+
+
+            })
+        } else {
+            alert('Данные не валидны!!!')
+        }
+
+
     })
 }
 export default sendForm
